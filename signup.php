9eb38336e74c767d87299caa4a00feb360cdc24b
Sharad Shinde <shindesharad71@gmail.com>
2016-03-05 22:26:36 +0530

    code updated

--- signup.php
+++ signup.php
@@ -26,9 +26,21 @@
 		$q = "INSERT into students (name, email, username, password, pic, nick) VALUES ('$name','$email','$username','$password','img/user.jpg', '$nick')";
 
 		mysqli_query($con,$q);
-		echo '<br><br><h3 class="text-center">redirecting to <a href="profile.php">login</a> page!</h3>';
-		echo '<script>setTimeout(function () { window.location.href = "index.php";}, 2000);</script>';
+		$rows = mysqli_affected_rows($con);
+
+		if($rows == 1)
+		{
+			echo '<br><br><h3 class="text-center">Registered successfully!<br>redirecting to <a href="profile.php">login</a> page!</h3>';
+			echo '<script>setTimeout(function () { window.location.href = "index.php";}, 2000);</script>';
+		}
+		else
+		{
+			echo 'signup error';
+		}
+		
 	}
+	else
+		echo 'error in sign up';
 
 	mysqli_close($con);
 ?>
